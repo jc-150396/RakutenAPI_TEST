bf589e0d34c48e8568c42b836659798fe788d678
瑞基 伊藤 <150396@jc-21.jp>
2017-12-07 12:57:54 +0900

    初回

---
+++ RakutenAPI_App33/RakutenAPI_App33/RakutenModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace RakutenAPI_App33
+{
+    public class RakutenModel
+    {
+        [DataContract]
+        public class ServiceResult
+        {
+            [DataMember]
+            public int count { get; set; }
+            [DataMember]
+            public int page { get; set; }
+            [DataMember]
+            public string first { get; set; }
+            [DataMember]
+            public int last { get; set; }
+            [DataMember]
+            public int hits { get; set; }
+            [DataMember]
+            public int carrier { get; set; }
+            [DataMember]
+            public int pageCount { get; set; }
+
+            [DataMember]
+            public List<Item> Items { get; set; }
+
+            [DataContract]
+            public class Item
+            {
+                [DataMember]
+                public string title { get; set; }
+                [DataMember]
+                public string titleKana { get; set; }
+                [DataMember]
+                public string subTitle { get; set; }
+                [DataMember]
+                public string subTitleKana { get; set; }
+                [DataMember]
+                public string seriesName { get; set; }
+                [DataMember]
+                public string seriesNameKana { get; set; }
+                [DataMember]
+                public string contents { get; set; }
+                [DataMember]
+                public string author { get; set; }
+                [DataMember]
+                public string authorKana { get; set; }
+                [DataMember]
+                public string publisherName { get; set; }
+                [DataMember]
+                public int size { get; set; }
+                [DataMember]
+                public string isbn { get; set; }
+                [DataMember]
+                public string itemCaption { get; set; }
+                [DataMember]
+                public string salesDate { get; set; }
+                [DataMember]
+                public int itemPrice { get; set; }
+                [DataMember]
+                public int listPrice { get; set; }
+                [DataMember]
+                public float discountRate { get; set; }
+                [DataMember]
+                public int discountPrice { get; set; }
+                [DataMember]
+                public string itemUrl { get; set; }
+                [DataMember]
+                public string affiliateUrl { get; set; }
+                [DataMember]
+                public string smallImageUrl { get; set; }
+                [DataMember]
+                public string mediumImageUrl { get; set; }
+                [DataMember]
+                public string largeImageUrl { get; set; }
+                [DataMember]
+                public string chirayomiUrl { get; set; }
+                [DataMember]
+                public int availability { get; set; }
+                [DataMember]
+                public int postageFlag { get; set; }
+                [DataMember]
+                public int limitedFlag { get; set; }
+                [DataMember]
+                public float reviewcount { get; set; }
+                [DataMember]
+                public float reviewAverage { get; set; }
+                [DataMember]
+                public string booksGenreId { get; set; }
+
+            }
+
+
+        } 
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ RakutenAPI_App33/RakutenAPI_App33/WebData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using Xamarin.Forms;
+
+namespace RakutenAPI_App33
+{
+    public class WebData : ContentPage
+    {
+        public List<RakutenModel> RakutenList;
+
+        public string Rakuten_URL = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?format=json&isbn=|&booksGenreId=001&applicationId=1008026300680682252";
+
+        public async Task<List<RakutenModel>> AsyncGetWebData()
+        {
+
+            //ISBNコード入力
+            var isbncode = ;
+
+
+            RakutenList = new List<RakutenModel>();
+
+            HttpClient httpClient = new HttpClient();
+
+            Task<string> stringAsync = httpClient.GetStringAsync(Rakuten_URL);
+
+            string result = await stringAsync;
+
+            RakutenList = JsonConvert.DeserializeObject<List<RakutenModel>>(result);
+
+            return RakutenList;
+
+        }
+    }
+}
